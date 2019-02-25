92cd9d0e1688b926c1076f8b477791507800d690
guillainbisimwa <guillainbisimwa@hotmail.fr>
2019-02-25 07:46:15 -0500

    Finish answering the first question

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import math
 
 df2 = pd.read_csv(
     'Data_ DRC.1.csv')
@@ -22,57 +24,108 @@ def generate_table(dataframe): #max_rows=5
         ]) for i in range(min(len(dataframe), len(dataframe)))]
     )
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+def result_over_time():
+    return (
+        dcc.Graph(
+            id='rot-graph',
+            figure={
+                'data': [
+                     go.Scatter(
+                                    x = [ report_date for report_date in df["report_date"].unique() ],
+                                    y = [cc for cc in df["confirmed_deaths"]],
+                                    line = {"color": "rgb(53, 83, 255)"},
+                                    mode = "lines",
+                                    name = "confirmed_deaths"
+                                ),
+                    # {
+                    #     'x': [ report_date for report_date in df["report_date"].unique() ],
+                    #     'y': [cc for cc in df["confirmed_deaths"]],
+                    #     'type': 'bar', 'name': 'confirmed_deaths'
+                    # },
+                   
+                ],
+                'layout': {
+                    'title': 'Dash Data Visualization'
+                }
+            }
+        )
+    )
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+external_scripts = [
+    'assets/material.min.js'
+]
+
+# external CSS stylesheets
+external_stylesheets = [
+    'assets/material.min.css',
+    'assets/bWLwgP.css'
+]
+
+app = dash.Dash(__name__,
+                external_scripts=external_scripts,
+                external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H3(children='The epidemiological situation of the Ebola Virus Disease from 2018-08-04 to 2019-02-21'),
-    # generate_table(df),
-    html.Label('Selectionner la datte'),
-    dcc.Dropdown(
-        id='datte-id',
-        options=[{'label': report_date, 'value': report_date} for report_date in df['report_date'].unique()],
-        value='2019-02-20'
+
+    # header
+    html.Div([
+        html.H3("The epidemiological situation of the Ebola Virus (DRC) from 2018-08-04 to 2019-02-21", 
+        className='mdl-layout--large-screen-only mdl-pad-to-bottom-50'),
+        ],
+        className="m-botm-20 mdl-layout__header mdl-color--pink-800"
     ),
-    html.Div(id='datte-div'),
-    html.H2(children='''First question.'''),
-    html.Div(children="(10 zones de sante les plus touchees depuis l'epidemie d'ebola)"),
-    # dcc.Graph(
-    #     id='example-graph',
-    #     figure={
-    #         'data': [
-    #             {
-    #                 'x': [health_zone for health_zone in df["health_zone"]],
-    #                 'y': [cc for cc in df["confirmed_cases"]],
-    #                 'type': 'bar', 'name': 'Confirmed cases'
-    #              },
-    #              {
-    #                 'x': [health_zone for health_zone in df["health_zone"]],
-    #                 'y': [cc for cc in df["confirmed_deaths"]],
-    #                 'type': 'bar', 'name': 'Confirmed deaths'
-    #              }
-                
-                
-    #         ],
-    #         'layout': {
-    #             'title': 'Dash Data Visualization'
-    #         }
-    #     }
-    # )
-])
+
+    # generate_table(df),
+    html.Section([
+        html.Div([
+            # html.Label('Selectionner la datte',
+            # className="mdl-cell mdl-cell--6-col"),
+            dcc.Dropdown(
+                id='datte-id',
+                options=[{'label': report_date, 'value': report_date} for report_date in df['report_date'].unique()],
+                value='2019-02-20',
+                className="mdl-my-input"
+            ),
+            html.Div(id='datte-div'),
+            html.P("\
+                The first graph allows you to see the situation at a desired date. \
+                And to understand which health zone is most affected in terms of confirmed cases \
+                and cases of death confirms.",
+                    className="mdl-my-input"
+                ),
+            ],
+            className="mdl-card mdl-cell mdl-cell--12-col"
+        ),
+        ],
+        className="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp my_section"
+    ),
+
+    html.Section([
+        html.Div([
+            result_over_time(),
+            ],
+            className="mdl-card mdl-cell mdl-cell--12-col"
+        ),
+        ],
+        className="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp my_section"
+    ),
+
+    # html.H2(children='''First question.'''),
+    # html.Div(children="(10 zones de sante les plus touchees depuis l'epidemie d'ebola)"),
+    #result_over_time()
+],
+className="mdl-color--grey-100")
 
 @app.callback(
     Output(component_id='datte-div', component_property='children'),
     [Input(component_id='datte-id', component_property='value')]
 )
-def update_output_div(selected_datte):
-    #return 'You\'ve entered "{}"'.format(selected_datte)
+def selected_datte_output_div(selected_datte):
+    #   return 'You\'ve entered "{}"'.format(selected_datte)
     filtered_df = df[df.report_date == selected_datte]
-
     return (
         dcc.Graph(
-            id='example-graph',
+            id='cc-by-cd-graph',
             figure={
                 'data': [
                     {
@@ -85,16 +138,13 @@ def update_output_div(selected_datte):
                         'y': [cc for cc in filtered_df["confirmed_deaths"]],
                         'type': 'bar', 'name': 'Confirmed deaths'
                     }
-                    
-                    
                 ],
                 'layout': {
-                    'title': 'Dash Data Visualization on "{}" '.format(selected_datte)
+                    'title': 'DRC Ebola Outbreak, North Kivu and Ituri - MOH-By-Health-Zone on {}'.format(selected_datte)
                 }
             }
         )
     )
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
